81471a26fe6b26658a92d2a699a2cce444655ce9
Andrew Martin <amartin@layer3com.com>
2016-05-18 12:23:58 -0400

    add functions for rendering a mac address

--- src/Net/Mac.hs
+++ src/Net/Mac.hs
@@ -12,6 +12,11 @@ import qualified Data.Attoparsec.Text as AT
 import qualified Data.Attoparsec.ByteString.Char8 as AB
 import Data.Bits ((.&.),(.|.),shiftR,shiftL,complement)
 import Net.Internal (attoparsecParseJSON)
+import qualified Data.Text.Lazy.Builder as TBuilder
+import Data.Text.Lazy.Builder.Int (hexadecimal)
+import Data.Monoid ((<>))
+import qualified Data.Aeson as Aeson
+import qualified Data.Text.Lazy as LText
 
 data Mac = Mac
   { macA :: {-# UNPACK #-} !Word16
@@ -21,8 +26,29 @@ data Mac = Mac
 
 instance Hashable Mac
 
+instance ToJSON Mac where
+  toJSON = Aeson.String . toText
+
 instance FromJSON Mac where
   parseJSON = attoparsecParseJSON (textParser <* AT.endOfInput)
+
+toText :: Mac -> Text
+toText = LText.toStrict . TBuilder.toLazyText . toTextBuilder
+
+toTextBuilder :: Mac -> TBuilder.Builder
+toTextBuilder (Mac a b) = 
+  hexadecimal (255 .&. shiftR a 8 )
+  <> colon
+  <> hexadecimal (255 .&. a )
+  <> colon
+  <> hexadecimal (255 .&. shiftR b 24 )
+  <> colon
+  <> hexadecimal (255 .&. shiftR b 16 )
+  <> colon
+  <> hexadecimal (255 .&. shiftR b 8 )
+  <> colon
+  <> hexadecimal (255 .&. b)
+  where colon = TBuilder.singleton ':'
 
 -- | This does not do an endOfInput check
 textParser :: AT.Parser Mac
