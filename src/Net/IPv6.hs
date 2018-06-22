d283911741ded4ef90b0744b7c6451b1b1300d2f
George Pollard <porges@porg.es>
2018-06-22 16:32:33 +1000

    Rewrite IPv6 parser to accept decimal notation

--- src/Net/IPv6.hs
+++ src/Net/IPv6.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE MagicHash           #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnboxedTuples       #-}
@@ -30,6 +31,7 @@ module Net.IPv6
   , print
   ) where
 
+import Net.IPv4 (IPv4(..))
 import qualified Net.IPv4 as IPv4
 
 import Control.Applicative
@@ -303,102 +305,83 @@ decode :: Text -> Maybe IPv6
 decode t = rightToMaybe (AT.parseOnly (parser <* AT.endOfInput) t)
 
 parser :: Atto.Parser IPv6
-parser = do
-  s <- start
-  case toIPv6 s of
-    Nothing -> fail "Wrong number of octets in IPv6 address"
-    Just ip -> return ip
+parser = startIP >>= makeIP
   where
-  msg = "All chunks in a formatted IPv6 address must be between 0x0000 and 0xFFFF"
-  colonMsg = "Cannot use double colon for omitting zeroes more than once in an IPv6 address"
-  start = do
-    c <- Atto.peekChar'
-    if c == ':'
-      then go (-1) 0 []
-      else Atto.hexadecimal >>= \w -> go (-1) 1 [w]
-    -- r <- fmap Just Atto.hexadecimal <|> (Nothing <$ Atto.char ':')
-    -- case r of
-    --   Just !w -> go (-1) 1 [w]
-    --   Nothing -> go 0 0 []
-  go !colonIndex !currentIndex !ws = do
-    r <- do
-      m <- Atto.peekChar
-      case m of
-        Nothing -> return ResDone
-        Just c -> if c == ':'
-          then do
-            _ <- Atto.anyChar -- should be a colon
-            if colonIndex == currentIndex
-              then fmap ResWord Atto.hexadecimal <|> pure ResDone
-              else do
-                d <- Atto.peekChar'
-                if d == ':'
-                  then return ResColon
-                  else fmap ResWord Atto.hexadecimal
-          else return ResDone
-    case r of
-      ResDone -> pure (S colonIndex currentIndex ws)
-      ResColon -> if alreadySet colonIndex
-        then fail colonMsg
-        else go currentIndex currentIndex ws
-      ResWord w -> restrictTo16 msg w >> go colonIndex (currentIndex + 1) (w : ws)
+  -- handles the case where an IP starts with ::
+  startIP :: Atto.Parser ([Word16], Maybe [Word16])
+  startIP = 
+    (\ends -> ([], Just ends)) <$> (Atto.char ':' *> Atto.char ':' *> restOfIP []) <|>
+    fullIP []
 
-toIPv6 :: S -> Maybe IPv6
-toIPv6 (S colonIndex total input) = case compare total 8 of
-  EQ -> if colonIndex == (-1)
-    then go 0 0 input
-    else Nothing
-  GT -> Nothing
-  LT -> go 0 0 input
-  where
-  revColonIndex = total - colonIndex
-  spacesToSkip = 8 - total
-  go :: Int -> Word64 -> [Word64] -> Maybe IPv6
-  go !ix !acc ws = if ix > 3
-    then fmap (flip IPv6 acc) (go2 ix 0 ws)
-    else case ws of
-      w : wsNext -> if ix == revColonIndex
-        then go (ix + spacesToSkip) acc (w : wsNext)
-        else go (ix + 1) (acc .|. unsafeShiftL w (times16 ix)) wsNext
-      [] -> if ix == revColonIndex
-        then Just $ IPv6 0 acc
-        else Nothing -- Not enough word16s in list
-  go2 :: Int -> Word64 -> [Word64] -> Maybe Word64
-  go2 !ix !acc ws = case ws of
-    w : wsNext -> if ix == revColonIndex
-      then go2 (ix + spacesToSkip) acc (w : wsNext)
-      else go2 (ix + 1) (acc .|. unsafeShiftL w (times16 ix - 64)) wsNext
-    [] -> if ix == revColonIndex || ix > 7
-      then Just acc
-      else Nothing -- Not enough word16s in list
+  -- a full IP that might contain ::
+  fullIP :: [Word16] -> Atto.Parser ([Word16], Maybe [Word16])
+  fullIP starts =
+    ((\x -> (x ++ starts, Nothing)) <$> ipv4) <|>
+    startPart starts
 
-times16 :: Int -> Int
-times16 a = unsafeShiftL a 4
+  -- an IP that cannot contain ::
+  restOfIP :: [Word16] -> Atto.Parser [Word16]
+  restOfIP ends =
+    ((\x -> x ++ ends) <$> ipv4) <|>
+    endPart ends <|>
+    pure ends
+  
+  ipv4 = ipv4ToWord16s <$> IPv4.parser
+  ipv4ToWord16s (IPv4 ip) = [fromIntegral (ip .&. 0xFFFF), fromIntegral (ip `unsafeShiftR` 16)]
+  
+  -- a colon-separated part before ::
+  startPart (starts) = do
+    part <- Atto.hexadecimal
+    let result = (part : starts)
+    Atto.peekChar >>= \case 
+      Just ':' -> do
+        _ <- Atto.anyChar -- will be ':'
+        Atto.peekChar >>= \case
+          Just ':' -> do
+            _ <- Atto.anyChar -- will be ':' 
+            (\ends -> (result, Just ends)) <$> restOfIP []
+          _ ->
+            fullIP result
+      _ ->
+        pure (result, Nothing)
 
-alreadySet :: Int -> Bool
-alreadySet i = i /= (-1)
+  -- a colon-separated part after ::
+  endPart ends = do
+    part <- Atto.hexadecimal
+    let result = part : ends
+    Atto.peekChar >>= \case 
+      Just ':' -> do
+        _ <- Atto.anyChar -- will be ':'
+        Atto.peekChar >>= \case
+          Just ':' -> do
+            fail "Cannot use double colon for omitting zeroes more than once in an IPv6 address"
+          _ ->
+            restOfIP result
+      _ ->
+        pure result
 
-restrictTo16 :: String -> Word64 -> Atto.Parser ()
-restrictTo16 msg w = if w > 65535
-  then fail msg
-  else return ()
-
--- | This is an internal data type used as the result
---   after parsing an ipv6 address. The first field
---   indicates the index at which a double colon occurs.
---   The second is the length of the third.
---   The third is a reversed list of the 16s
---   that comprise the ipv6 address.
-data S = S
-  { _sDoubleColon :: {-# UNPACK #-} !Int
-  , _sTotal :: {-# UNPACK #-} !Int
-  , _sRevWords :: ![Word64]
-  } deriving (Show,Read)
-
-data Res
-  = ResWord {-# UNPACK #-} !Word64
-  | ResColon
-  | ResDone
+  makeIP :: ([Word16], Maybe [Word16]) -> Atto.Parser IPv6
+  makeIP (starts, mends) =
+    case mends of 
+      -- Nothing indicates we never encountered double-colon, so we must have
+      -- all 8 parts:
+      Nothing -> do
+        if numStarts /= 8
+        then fail "not enough colon-separated parts in IPv6 address"
+        else
+          let [w1, w2, w3, w4, w5, w6, w7, w8] = reverse starts in
+          pure (fromWord16s w1 w2 w3 w4 w5 w6 w7 w8)
+      -- otherwise, we did encounter a double-colon, so we expand it to fill:
+      Just ends ->
+        let numEnds = length ends in
+        case compare (numStarts + numEnds) 8 of
+          GT -> fail "too many colon-separated parts in IPv6 address"
+          EQ -> fail "unnecessary double-colon in IPv6 address"
+          LT -> 
+            let [w1, w2, w3, w4, w5, w6, w7, w8] = reverse starts ++ replicate (8 - (numStarts + numEnds)) 0 ++ reverse ends in
+            pure (fromWord16s w1 w2 w3 w4 w5 w6 w7 w8)
+    where
+    numStarts = length starts
 
 fromOctetsV6 ::
      Word64 -> Word64 -> Word64 -> Word64
