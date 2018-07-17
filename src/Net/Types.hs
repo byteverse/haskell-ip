7fd02510c678fc6e3bea450ec83c08e8719b9260
Andrew Martin <andrew.thaddeus@gmail.com>
2018-07-17 16:46:36 -0400

    add some tests for IPv6 ranges

--- src/Net/Types.hs
+++ src/Net/Types.hs
@@ -5,12 +5,13 @@ module Net.Types
   , IPv6(..)
   , IP(..)
   , IPv4Range(..)
+  , IPv6Range(..)
   , Mac(..)
   , MacCodec(..)
   , MacGrouping(..)
   ) where
 
-import Net.IPv6 (IPv6(..))
+import Net.IPv6 (IPv6(..),IPv6Range(..))
 import Net.IPv4 (IPv4(..), IPv4Range(..))
 import Net.IP (IP(..))
 import Net.Mac (Mac(..),MacCodec(..),MacGrouping(..))
