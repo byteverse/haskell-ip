9569f951e63b4bff104a0a001fe366b7ee8d9837
Drew Hess <src@drewhess.com>
2019-10-28 20:07:59 +0000

    Remove Integral, Num, and Real instances for IPv6.
    These are nonsensical for an IPv6 value.

--- src/Net/IPv6.hs
+++ src/Net/IPv6.hs
@@ -119,7 +119,7 @@ import qualified Net.IPv4 as IPv4
 
 -- | A 128-bit Internet Protocol version 6 address.
 newtype IPv6 = IPv6 { getIPv6 :: Word128 }
-  deriving (Bounded,Enum,Eq,Integral,Num,Ord,Real,Storable,Bits,FiniteBits,NFData,Prim,Ix,Data)
+  deriving (Bounded,Enum,Eq,Ord,Storable,Bits,FiniteBits,NFData,Prim,Ix,Data)
 
 instance Show IPv6 where
   showsPrec p addr = showParen (p > 10)
